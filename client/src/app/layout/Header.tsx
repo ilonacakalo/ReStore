835639a04db973acdcf7b2bc95ce56a2bdfeb5db
ilonacakalo <ilona.cakalo@gmail.com>
2024-03-09 16:39:47 +0100

    End of section 3

---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,17 @@
+import { AppBar, Switch, Typography } from "@mui/material";
+
+interface Props {
+    darkMode: boolean;
+    handleThemeChange: () => void;
+}
+
+export default function Header ({darkMode, handleThemeChange}: Props){
+    return(
+        <AppBar position="static" sx={{mb: 4}}>
+            <Typography variant="h6">
+                RE-STORE
+            </Typography>
+            <Switch checked={darkMode} onChange={handleThemeChange}/>
+        </AppBar>
+    )
+}
